48c80a33f16ca208c38a697dc0094f0e2299f39b
Daniel Alberto Vizcarra Gaibor <thedavizg@gmail.com>
2025-05-13 19:38:21 -0500

    Create movie functionality for admin with validations and genre selection

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
     has_and_belongs_to_many :genres
-end
+  
+    validates :title, presence: true # Asegura que el título no esté vacío
+    validates :release_year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1880, less_than_or_equal_to: Date.current.year + 5 } # Asegura que el año sea un número y esté en un rango razonable
+  end
